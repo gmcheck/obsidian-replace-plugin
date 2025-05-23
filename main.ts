b35579b6afab6a37b8fcbd464751d5ccd1da414d
jqwu <jqwu@sstir.cn>
2025-05-23 21:02:24 +0800

    'init'

--- main.ts
+++ main.ts
@@ -1,134 +1,161 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
 
-// Remember to rename these classes and interfaces!
+import { Plugin, App, Editor, MarkdownView, Modal, Setting, PluginSettingTab } from 'obsidian';
 
-interface MyPluginSettings {
-	mySetting: string;
+interface FindReplaceSettings {
+    searchPattern: string;
+    replacePattern: string;
+    useRegex: boolean;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+const DEFAULT_SETTINGS: FindReplaceSettings = {
+    searchPattern: '',
+    replacePattern: '',
+    useRegex: false
+};
+
+export default class FindReplacePlugin extends Plugin {
+    settings: FindReplaceSettings;
+
+    async onload() {
+        await this.loadSettings();
+        
+        this.addCommand({
+            id: 'find-replace-current-file',
+            name: 'Find and Replace in Current File',
+            hotkeys: [{ modifiers: ["Mod", "Shift"], key: "f" }],
+            callback: () => {
+                new FindReplaceModal(this.app, this).open();
+            }
+        });
+
+        this.addSettingTab(new FindReplaceSettingTab(this.app, this));
+    }
+
+    async loadSettings() {
+        this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+    }
+
+    async saveSettings() {
+        await this.saveData(this.settings);
+    }
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+class FindReplaceModal extends Modal {
+    plugin: FindReplacePlugin;
+    searchPattern: string;
+    replacePattern: string;
+    useRegex: boolean;
 
-	async onload() {
-		await this.loadSettings();
+    constructor(app: App, plugin: FindReplacePlugin) {
+        super(app);
+        this.plugin = plugin;
+        this.searchPattern = this.plugin.settings.searchPattern;
+        this.replacePattern = this.plugin.settings.replacePattern;
+        this.useRegex = this.plugin.settings.useRegex;
+    }
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+    onOpen() {
+        const { contentEl } = this;
+        contentEl.createEl('h2', { text: 'Find and Replace' });
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+        new Setting(contentEl)
+            .setName('Search Pattern')
+            .addText(text => text
+                .setValue(this.searchPattern)
+                .onChange(value => this.searchPattern = value));
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
+        new Setting(contentEl)
+            .setName('Replace Pattern')
+            .addText(text => text
+                .setValue(this.replacePattern)
+                .onChange(value => this.replacePattern = value));
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
+        new Setting(contentEl)
+            .setName('Use Regular Expression')
+            .addToggle(toggle => toggle
+                .setValue(this.useRegex)
+                .onChange(value => this.useRegex = value));
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+        new Setting(contentEl)
+            .addButton(btn => btn
+                .setButtonText('Replace All')
+                .setCta()
+                .onClick(() => {
+                    this.replaceAll();
+                    this.close();
+                }));
+    }
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
+    replaceAll() {
+        const activeView = this.app.workspace.getActiveViewOfType(MarkdownView);
+        if (activeView) {
+            const editor = activeView.editor;
+            let text = editor.getValue();
+            
+            try {
+                if (this.useRegex) {
+                    text = text.replace(new RegExp(this.searchPattern, 'g'), this.replacePattern);
+                } else {
+                    const escaped = this.searchPattern.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+                    text = text.replace(new RegExp(escaped, 'g'), this.replacePattern);
+                }
+                
+                editor.setValue(text);
+                this.plugin.settings.searchPattern = this.searchPattern;
+                this.plugin.settings.replacePattern = this.replacePattern;
+                this.plugin.settings.useRegex = this.useRegex;
+                this.plugin.saveSettings();
+            } catch (e) {
+                console.error('Replace error:', e);
+            }
+        }
+    }
 
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
-
-	onunload() {
-
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
+    onClose() {
+        const { contentEl } = this;
+        contentEl.empty();
+    }
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+class FindReplaceSettingTab extends PluginSettingTab {
+    plugin: FindReplacePlugin;
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
+    constructor(app: App, plugin: FindReplacePlugin) {
+        super(app, plugin);
+        this.plugin = plugin;
+    }
 
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
+    display(): void {
+        const { containerEl } = this;
+        containerEl.empty();
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+        containerEl.createEl('h2', { text: 'Find and Replace Settings' });
 
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
+        new Setting(containerEl)
+            .setName('Default Search Pattern')
+            .addText(text => text
+                .setValue(this.plugin.settings.searchPattern)
+                .onChange(async (value) => {
+                    this.plugin.settings.searchPattern = value;
+                    await this.plugin.saveSettings();
+                }));
 
-	display(): void {
-		const {containerEl} = this;
+        new Setting(containerEl)
+            .setName('Default Replace Pattern')
+            .addText(text => text
+                .setValue(this.plugin.settings.replacePattern)
+                .onChange(async (value) => {
+                    this.plugin.settings.replacePattern = value;
+                    await this.plugin.saveSettings();
+                }));
 
-		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
+        new Setting(containerEl)
+            .setName('Default Use Regex')
+            .addToggle(toggle => toggle
+                .setValue(this.plugin.settings.useRegex)
+                .onChange(async (value) => {
+                    this.plugin.settings.useRegex = value;
+                    await this.plugin.saveSettings();
+                }));
+    }
 }
